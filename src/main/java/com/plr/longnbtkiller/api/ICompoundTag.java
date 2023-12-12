3c00795a74a00449c88435fcd015775271100ce3
MikhailTapio <mikhailtapio13468@163.com>
2023-12-12 16:27:47 +0800

    Port to Forge 1.20.4

--- src/main/java/com/plr/longnbtkiller/api/ICompoundTag.java
+++
@@ -1,9 +0,0 @@
-package com.plr.longnbtkiller.api;
-
-import net.minecraft.nbt.Tag;
-
-import java.util.Map;
-
-public interface ICompoundTag {
-    void longnbtkiller$setTags(Map<String, Tag> tags);
-}
--- src/main/java/com/plr/longnbtkiller/api/IListTag.java
+++
@@ -1,9 +0,0 @@
-package com.plr.longnbtkiller.api;
-
-import net.minecraft.nbt.Tag;
-
-import java.util.List;
-
-public interface IListTag {
-    void longnbtkiller$setListAndType(List<Tag> list, byte type);
-}
