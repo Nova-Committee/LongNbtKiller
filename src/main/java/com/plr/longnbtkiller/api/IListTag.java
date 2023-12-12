c441a8524cb10426d6a96dfaa15c85aaeb3f856b
MikhailTapio <mikhailtapio13468@163.com>
2023-12-12 15:39:00 +0800

    Port to Fabric 1.20.1

---
+++ src/main/java/com/plr/longnbtkiller/api/IListTag.java
@@ -0,0 +1,9 @@
+package com.plr.longnbtkiller.api;
+
+import net.minecraft.nbt.Tag;
+
+import java.util.List;
+
+public interface IListTag {
+    void longnbtkiller$setListAndType(List<Tag> list, byte type);
+}
--- src/main/java/com/plr/longnbtkiller/mixin/MixinFriendlyByteBuf.java
+++ src/main/java/com/plr/longnbtkiller/mixin/MixinFriendlyByteBuf.java
@@ -1,15 +1,22 @@
 package com.plr.longnbtkiller.mixin;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtAccounter;
 import net.minecraft.network.FriendlyByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 
 @Mixin(FriendlyByteBuf.class)
 public abstract class MixinFriendlyByteBuf {
-    @Redirect(method = "readNbt()Lnet/minecraft/nbt/CompoundTag;", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NbtAccounter;create(J)Lnet/minecraft/nbt/NbtAccounter;"))
-    private NbtAccounter inject$readNbt(long l) {
-        return NbtAccounter.unlimitedHeap();
+    @Shadow
+    public abstract CompoundTag readNbt(NbtAccounter accounter);
+
+    @Inject(method = "readNbt()Lnet/minecraft/nbt/CompoundTag;", at = @At("HEAD"), cancellable = true)
+    private void inject$readNbt(CallbackInfoReturnable<CompoundTag> cir) {
+        cir.setReturnValue(this.readNbt(NbtAccounter.UNLIMITED));
     }
 }
