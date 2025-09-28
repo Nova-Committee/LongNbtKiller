424d4d9737451f7f39eac555565b8865998d9b81
北斗余晖 <81358657+MikhailTapio@users.noreply.github.com>
2025-09-28 18:28:58 +1000

    :tada: Opt

--- src/main/java/com/plr/longnbtkiller/LongNbtKiller.java
+++ src/main/java/com/plr/longnbtkiller/LongNbtKiller.java
@@ -1,12 +1,6 @@
 package com.plr.longnbtkiller;
 
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.config.ModConfigEvent;
-import net.neoforged.neoforge.common.ModConfigSpec;
 
 @Mod(LongNbtKiller.MODID)
 public class LongNbtKiller {
